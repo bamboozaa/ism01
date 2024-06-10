b7499a539fafff8cae6ec50cf1d10be214b0e5cf
bamboozaa <chukadee@gmail.com>
2024-06-10 10:47:02 +0700

    PC Commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->to('https://ism.utcc.ac.th');
 });
