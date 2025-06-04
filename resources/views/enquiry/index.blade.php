6cabbdf839a49b23269aab536ae10ff913dc2d13
bamboozaa <chukadee@gmail.com>
2025-06-04 16:13:06 +0700

    PC Commit

--- resources/views/enquiry/index.blade.php
+++ resources/views/enquiry/index.blade.php
@@ -17,6 +17,7 @@
             });
         @endif
 
+
         // @if (session('deleted'))
         //     Swal.fire({
         //         title: "Are you sure?",
@@ -60,7 +61,7 @@
                 @foreach ($enquiries as $key => $enquiry)
                     <tr>
                         <td>{{ $key + 1 }}</td>
-                        <td class="text-nowrap"><a href="{{ route('enquiries.show', $enquiry['enq_id']) }}"
+                        <td class="text-nowrap"><a href="{{ route('enquiries.show', $enquiry) }}"
                                 target="_blank">{{ $enquiry['name'] }}</a></td>
                         <td>{{ $enquiry['direct_message'] }}</td>
                         <td>{{ $enquiry['nationality'] }}</td>
@@ -72,14 +73,14 @@
                                     <i class="bi bi-three-dots-vertical"></i>
                                 </button>
                                 <div class="dropdown-menu dropdown-menu-end" style="">
-                                    <a class="dropdown-item text-info"
-                                        href="{{ route('enquiries.show', $enquiry['enq_id']) }}" data-coreui-i18n="info"
-                                        target="_blank">{{ __('Info') }}</a>
-                                    <form action="{{ route('enquiries.destroy', $enquiry['enq_id']) }}" method="post">
+                                    <a class="dropdown-item text-info" href="{{ route('enquiries.show', $enquiry) }}"
+                                        data-coreui-i18n="info" target="_blank">{{ __('Info') }}</a>
+                                    <form id="delete-form-{{ $enquiry['enq_id'] }}"
+                                        action="{{ route('enquiries.destroy', $enquiry) }}" method="post">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="dropdown-item text-danger">Delete</button>
                                     </form>
+                                    <button type="button" onclick="confirmDelete({{ $enquiry['enq_id'] }})" class="dropdown-item text-danger">Delete</button>
                                 </div>
                             </div>
                         </td>
@@ -93,5 +94,23 @@
             @endif
         </tbody>
     </table>
+
+    <script>
+        function confirmDelete(id) {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('delete-form-' + id).submit();
+                }
+            });
+        }
+    </script>
 
 @endsection
